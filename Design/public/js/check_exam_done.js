a666e5de3dc866d22dfc3182f02b1b7f32bba39d
Tai Tran <tranthanhtai257@gmail.com>
2018-12-05 01:44:54 +0700

    Tạo giao diên đề thi, hiển thi kết quả và xếp hạng

---
+++ Design/public/js/check_exam_done.js
@@ -0,0 +1,91 @@
+function myFunction1(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[0].style.background = '#d9edfe';
+      }
+    }
+ }
+function my2(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[1].style.background = '#d9edfe';
+      }
+    }
+ }
+function my3(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[2].style.background = '#d9edfe';
+      }
+    }
+ }
+function my4(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[3].style.background = '#d9edfe';
+      }
+    }
+ }
+function my5(){
+    var x = document.querySelectorAll("div.box");
+    var tagInput = document.getElementsByTagName("input");
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[4].style.background = "#d9edfe";
+      }
+    }
+ }
+function my6(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[5].style.background = '#d9edfe';
+      }
+    }
+ }
+ function my7(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[6].style.background = '#d9edfe';
+      }
+    }
+ }
+ function my8(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[7].style.background = '#d9edfe';
+      }
+    }
+ }
+ function my9(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[8].style.background = '#d9edfe';
+      }
+    }
+ }
+ function my10(){
+    var x = document.querySelectorAll('div.box');
+    var tagInput = document.getElementsByTagName('input');
+    for(var i=0;i<3;i++){
+      if (tagInput[i].checked == true){
+        x[9].style.background = '#d9edfe';
+      }
+    }
+ }
+ 
